109b084efe73af80a72746cf173bbf0e1d338c10
pk <prashant003kareliya@gmail.com>
2025-09-13 16:25:06 +0530

    mac push

--- lib/module/dashboards/Technician/technician_dashboard_setup.dart
+++ lib/module/dashboards/Technician/technician_dashboard_setup.dart
@@ -16,6 +16,7 @@ import 'package:medizii/module/dashboards/Technician/bloc/technician_bloc.dart';
 import 'package:medizii/module/dashboards/Technician/data/technician_datasource.dart';
 import 'package:medizii/module/dashboards/Technician/data/technician_repository.dart';
 import 'package:medizii/module/dashboards/Technician/model/tc_accept_reject_request.dart';
+import 'package:medizii/module/dashboards/Technician/model/tc_accept_reject_response.dart';
 import 'package:medizii/module/dashboards/Technician/tc_home/tc_home_pg.dart';
 import 'package:medizii/module/dashboards/Technician/tc_patient/tc_patient_pg.dart';
 import 'package:medizii/module/dashboards/Technician/tc_setting/tc_setting_pg.dart';
@@ -132,7 +133,6 @@ class _TechnicianDashboardState extends State<TechnicianDashboard> {
 
   void _showAcceptDialog(dynamic data) {
     final bookingId = data['bookingId'];
-    final technicianId = prefs.getString(PreferenceString.prefsUserId);
 
     PlatformAwareDialog.show(
       context: context,
@@ -153,8 +153,8 @@ class _TechnicianDashboardState extends State<TechnicianDashboard> {
           technicianId: prefs.getString(PreferenceString.prefsUserId).toString(),
         );
         technicianBloc.add(EmsBookingAcceptEvent(technicianAcceptRejectRequest));
-        /*socket.emit("accept_booking", {"technicianId": prefs.getString(PreferenceString.prefsUserId), "bookingId": bookingId});
-        if (technicianId != null && bookingId != null) {
+        //socket.emit("booking_confirmed", {"technicianId": prefs.getString(PreferenceString.prefsUserId), "bookingId": bookingId});
+        /*if (technicianId != null && bookingId != null) {
           socket.emit("accept_booking", {
             "technicianId": technicianId,
             "bookingId": bookingId
@@ -173,62 +173,88 @@ class _TechnicianDashboardState extends State<TechnicianDashboard> {
     super.dispose();
   }
 
+  TechnicianAcceptRejectResponse? technicianAcceptRejectResponse;
+
   @override
   Widget build(BuildContext context) {
     final int currentIndex = context.watch<BottomNavProvider>().currentIndex;
 
     return Scaffold(
       body: _screens[currentIndex],
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          boxShadow: [BoxShadow(color: Colors.black.withValues(alpha: 0.1), blurRadius: 10, offset: const Offset(0, -2))],
-        ),
-        child: BottomNavigationBar(
-          iconSize: 25.r,
-          currentIndex: currentIndex,
-          type: BottomNavigationBarType.fixed,
+      bottomNavigationBar: BlocConsumer<TechnicianBloc, TechnicianState>(
+        bloc: technicianBloc,
+        listener: (context, state) {
+          if (state is LoadedState) {
+            technicianAcceptRejectResponse = state.data;
+            socket.emit("booking_confirmed", {
+              "bookingId": technicianAcceptRejectResponse?.booking?.sId,
+              "technician": prefs.getString(PreferenceString.prefsUserId),
+              "hospital": technicianAcceptRejectResponse?.booking?.hospitalId,
+              "message": "Your ambulance is on the way 🚑",
+            });
+          }
+        },
+        builder: (context, state) {
+          return Container(
+            decoration: BoxDecoration(
+              boxShadow: [BoxShadow(color: Colors.black.withValues(alpha: 0.1), blurRadius: 10, offset: const Offset(0, -2))],
+            ),
+            child: BottomNavigationBar(
+              iconSize: 25.r,
+              currentIndex: currentIndex,
+              type: BottomNavigationBarType.fixed,
 
-          onTap: (index) {
-            // Refresh logic: Even if the same index is tapped, force refresh
-            final provider = context.read<BottomNavProvider>();
-            if (provider.currentIndex != index) {
-              provider.setIndex(index);
-            } else {
-              provider.setIndex(index); // trigger rebuild intentionally
-            }
-          },
-          selectedItemColor: AppColors.blueColor,
-          unselectedItemColor: AppColors.gray,
-          selectedLabelStyle: GoogleFonts.dmSans(
-            textStyle: TextStyle(fontWeight: FontWeight.w500, fontSize: 12.sp, color: AppColors.textSecondary),
-          ),
-          unselectedLabelStyle: GoogleFonts.dmSans(
-            textStyle: TextStyle(fontWeight: FontWeight.w500, fontSize: 12.sp, color: AppColors.textSecondary),
-          ),
+              onTap: (index) {
+                // Refresh logic: Even if the same index is tapped, force refresh
+                final provider = context.read<BottomNavProvider>();
+                if (provider.currentIndex != index) {
+                  provider.setIndex(index);
+                } else {
+                  provider.setIndex(index); // trigger rebuild intentionally
+                }
+              },
+              selectedItemColor: AppColors.blueColor,
+              unselectedItemColor: AppColors.gray,
+              selectedLabelStyle: GoogleFonts.dmSans(
+                textStyle: TextStyle(fontWeight: FontWeight.w500, fontSize: 12.sp, color: AppColors.textSecondary),
+              ),
+              unselectedLabelStyle: GoogleFonts.dmSans(
+                textStyle: TextStyle(fontWeight: FontWeight.w500, fontSize: 12.sp, color: AppColors.textSecondary),
+              ),
 
-          items: [
-            BottomNavigationBarItem(
-              icon: _buildInActiveIcon(Assets.icIcons.home.svg()),
-              label: LabelString.labelHome,
-              activeIcon: _buildActiveIcon(Assets.icIcons.home.svg(colorFilter: ColorFilter.mode(AppColors.blueColor, BlendMode.srcIn))),
+              items: [
+                BottomNavigationBarItem(
+                  icon: _buildInActiveIcon(Assets.icIcons.home.svg()),
+                  label: LabelString.labelHome,
+                  activeIcon: _buildActiveIcon(
+                    Assets.icIcons.home.svg(colorFilter: ColorFilter.mode(AppColors.blueColor, BlendMode.srcIn)),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  icon: _buildInActiveIcon(Assets.icIcons.call.svg()),
+                  label: LabelString.labelPatients,
+                  activeIcon: _buildActiveIcon(
+                    Assets.icIcons.call.svg(colorFilter: ColorFilter.mode(AppColors.blueColor, BlendMode.srcIn)),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  icon: _buildInActiveIcon(Assets.icIcons.upload.svg()),
+                  label: LabelString.labelUploadReport,
+                  activeIcon: _buildActiveIcon(
+                    Assets.icIcons.upload.svg(colorFilter: ColorFilter.mode(AppColors.blueColor, BlendMode.srcIn)),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  icon: _buildInActiveIcon(Assets.icIcons.setting.svg()),
+                  label: LabelString.labelSetting,
+                  activeIcon: _buildActiveIcon(
+                    Assets.icIcons.setting.svg(colorFilter: ColorFilter.mode(AppColors.blueColor, BlendMode.srcIn)),
+                  ),
+                ),
+              ],
             ),
-            BottomNavigationBarItem(
-              icon: _buildInActiveIcon(Assets.icIcons.call.svg()),
-              label: LabelString.labelPatients,
-              activeIcon: _buildActiveIcon(Assets.icIcons.call.svg(colorFilter: ColorFilter.mode(AppColors.blueColor, BlendMode.srcIn))),
-            ),
-            BottomNavigationBarItem(
-              icon: _buildInActiveIcon(Assets.icIcons.upload.svg()),
-              label: LabelString.labelUploadReport,
-              activeIcon: _buildActiveIcon(Assets.icIcons.upload.svg(colorFilter: ColorFilter.mode(AppColors.blueColor, BlendMode.srcIn))),
-            ),
-            BottomNavigationBarItem(
-              icon: _buildInActiveIcon(Assets.icIcons.setting.svg()),
-              label: LabelString.labelSetting,
-              activeIcon: _buildActiveIcon(Assets.icIcons.setting.svg(colorFilter: ColorFilter.mode(AppColors.blueColor, BlendMode.srcIn))),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
